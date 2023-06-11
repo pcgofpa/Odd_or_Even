c8baeb979597ff21dfe32272a50c4f36a43485d1
pcgofpa <pcgofpa@gmail.com>
2023-06-11 08:46:41 -0500

    Update OddEven.py
    Output did not have punctuation.

--- OddEven.py
+++ OddEven.py
@@ -3,9 +3,9 @@ number = int(input("Which number do you want to check? "))
 #Determine if number is odd or even
 result = number % 2
 if result == 0:
-    message = "This number is even"
+    message = "This number is even."
 else:
-    message = "This number is odd"
+    message = "This number is odd."
 
 #Display whether number is even or odd    
 print(message)
